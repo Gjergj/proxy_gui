97df25e09e70d76213bda9c5a20177f5c8774920
gjergj <gjergjiramku@gmail.com>
2024-07-30 15:46:38 +0200

    improve api to better illustrate loadbalancing reverse proxy

--- testing/sample_api/main.go
+++ testing/sample_api/main.go
@@ -3,27 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	iName := os.Getenv("INSTANCE_NAME")
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /v1/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("created new user"))
+		w.Write([]byte(fmt.Sprintf("created new user %s", iName)))
 	})
 
 	router.HandleFunc("GET /v1/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("all users"))
+		w.Write([]byte(fmt.Sprintf("all users for %s", iName)))
 	})
 
 	router.HandleFunc("PATCH /v1/users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
-		w.Write([]byte(fmt.Sprintf("updated user %s", id)))
+		w.Write([]byte(fmt.Sprintf("updated user %s %s", id, iName)))
 	})
 
 	router.HandleFunc("DELETE /v1/users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
-		w.Write([]byte(fmt.Sprintf("deleted user %s", id)))
+		w.Write([]byte(fmt.Sprintf("deleted user %s %s", id, iName)))
 	})
 
 	http.ListenAndServe(":8080", router)
